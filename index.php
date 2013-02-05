86bea35ef3b63c8962362fa9c44b9e0e9969f6bb
cyril.thurier <cyril.thurier@gmail.com>
2013-02-05 18:31:04 +0000

    Cyril : proposition de structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LAPI.NET</title>
+	<meta name="author" content="master CCI">
+	<meta charset="utf-8">
+	<meta name="description" content="site de rencontre des lapinous">
+	<meta name="keywords" content="lapins rencontre date amitie amour">
+</head>
+<body>
+	<header><h1>LAPI.NET</h1></header>
+	<section><p>En construction...</p></section>
+	<footer>LAPI.NET</footer>
+</body>
+</html>
