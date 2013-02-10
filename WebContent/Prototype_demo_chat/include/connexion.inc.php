b105e2058d03a3e2a6245e48ae469d6c97d8a9f6
cyril.thurier <cyril.thurier@gmail.com>
2013-02-10 09:33:26 +0000

    mise a jour du prototype de tchat

---
+++ WebContent/Prototype_demo_chat/include/connexion.inc.php
@@ -0,0 +1,40 @@
+<?php
+	//////////////////////////////
+	// Librairie de fonctions   //
+	// utiles au chat           //
+	// pour l'envoi de messages //
+	// Cyril THURIER            //
+	//////////////////////////////
+
+	/*** ulterieurement en include config.inc.php ***/
+	$hostDB = 'localhost';
+	$userDB = 'root';
+	$passwd = 'caneton';
+	$dataBase = 'LAPI.NET';
+	$liendb = null;
+	/***************************************/
+	
+	/**************
+	 * connection *
+	 **************/
+	function connect(){
+		//variables globales
+		global $hostDB, $userDB, $passwd, $liendb, $dataBase;
+		 
+		//connection SGBD
+		$liendb = mysql_connect($hostDB,$userDB,$passwd);
+		if( ! mysql_select_db($dataBase) ){
+			return false;
+		}
+		return true;
+	}
+	/****************
+	 * deconnection *
+	 ****************/
+	function disconnect(){
+		//variables globales
+		global $liendb;
+		//fin connexion
+		mysql_close($liendb);
+	}
+?>
