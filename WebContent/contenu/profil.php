13b88df26962698ec6c8c67ab5a5b431789195f9
florent_arnould <florent_arnould@yahoo.fr>
2013-03-25 11:02:26 +0000

    màj mineures : commentaires et nettoyage

--- WebContent/contenu/profil.php
+++ WebContent/contenu/profil.php
@@ -82,6 +82,7 @@ if( $prive ){ //champs prives
 	echo "</form>\n";
 }else if( isset($_SESSION['identifiant']) ){ //profil public, etat connecte : proposer comme ami
 	echo "<a href=\"include/ajouter_ami.php?ami=".$user."\">ajouter aux amis</a>";
+	//inclusion du lien d'ouverture du formulaire de contact et de ce formulaire
 	include "include/messagerie/contact_profil.php";
 }
 //afficher les lapins ici
--- WebContent/include/messagerie/contact_profil.php
+++ WebContent/include/messagerie/contact_profil.php
@@ -1,41 +1,44 @@
 <?
-/*
-echo "<pre>";
-print_r($_REQUEST);
-print_r($_POST);
-print_r($_GET);
-print_r($_SESSION);
-print_r($personne);
-echo "</pre>";
-*/
+/*************************************************
+ *	Gère l'affichage du formulaire d'ouverture	**
+ *	d'une discussion avec un autre membre.		**
+ * 												**
+ * 		Florent Arnould	-	19 mars 2013		**
+ *************************************************/
 ?>
 <br />
 <a href="#" onclick="el=document.getElementById('fdisc');if (el.style.display=='block') el.style.display='none'; else el.style.display='block'; return false;">lancer une discussion</a>
 <div id="fdisc" style="display:none;">
 <form action="include/messagerie/ouvrir_disc.php" method="POST" name="contact">
-<fieldset>
-<legend>Nouvelle discussion</legend>
-<input type="hidden" name="page" value="profil"></input>
-<input type="hidden" name="id_profil" value="<? echo $personne['id_profil']; ?>"></input>
-<input type="hidden" name="mid" value="<? echo $_SESSION['mid']; ?>"></input>
+	<fieldset>
+		<legend>Nouvelle discussion</legend>
+		<input type="hidden" name="page" value="profil"></input>
+		<input type="hidden" name="id_profil" value="<? echo $personne['id_profil']; ?>"></input>
+		<input type="hidden" name="mid" value="<? echo $_SESSION['mid']; ?>"></input>
 auteur : <select name="auteur">
 <?
 
+//recherche des lapins du membre connecté
 $req="SELECT id_lapin, nomlap FROM `${prefixe}lapin` WHERE id_profil = '".$_SESSION['mid']."'";
 $res = requeteObj($req);
+//affichage sous forme d'options html
 foreach ($res as $lap)
 	echo "<option value='$lap->id_lapin'>$lap->nomlap</option>";
 
+//fermer et passer au destinataire
 ?>
 </select>
 &nbsp;&nbsp;destinataire : <select name="lapin">
 <?
 
+//recherche des lapins du profil consulté
 $req="SELECT id_lapin, nomlap FROM `${prefixe}lapin` WHERE identifiant = '$user'";
 $res = requeteObj($req);
+//affichage sous forme d'options html
 foreach ($res as $lap)
 	echo "<option value='$lap->id_lapin'>$lap->nomlap</option>";
 
+//fermer et passer au contenu du message
 ?>
 </select>
 <br />
